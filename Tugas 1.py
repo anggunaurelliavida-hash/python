c3f40041cd1ee6c016d5ab18bf1cee7de9a6365c
anggunaurelliavida-hash <anggunaurelliavida@gmail.com>
2025-08-23 11:55:13 +0700

    Add files via upload

---
+++ Tugas 1.py
@@ -0,0 +1,11 @@
+nama_produk1 = "kopi pagi"
+harga_produk1 = 18000.5
+nama_produk2 = "Roti Bakar"
+harga_produk2 = 12000
+status_Roti = True
+
+print(f"nama produk 1:", nama_produk1)
+print(f"harga produk 1:", harga_produk1)
+print(f"nama produk 2:", nama_produk2)
+print(f"harga produk 2:", harga_produk2) 
+print(f"status ketersediaan Roti:", status_Roti)
